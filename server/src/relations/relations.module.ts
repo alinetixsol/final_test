f023c175f6537c1a39134e2fb4c7921845ad25c5
alinetixsol <ali_muhammad_intern@netixsol.com>
2024-02-28 15:09:21 +0500

    Final

--- server/src/relations/relations.module.ts
+++ server/src/relations/relations.module.ts
@@ -3,16 +3,20 @@ import { RelationSchema, Relations } from './schemas/Relation.scheme';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RelationsService } from './relations.service';
 import { RelationsController } from './relations.controller';
-import { language, languageSchema } from './schemas/Language.scheme';
-import { Country, CountrySchema } from './schemas/Country.scheme';
+import { language, languageSchema } from '../relations/schemas/Language.scheme';
+import { Country, CountrySchema } from '../relations/schemas/Country.scheme';
 
 @Module({
-    imports :[
-        MongooseModule.forFeature([{name: Relations.name,  schema: RelationSchema}]),
-        MongooseModule.forFeature([{name: language.name,  schema: languageSchema}]),
-        MongooseModule.forFeature([{name: Country.name,  schema: CountrySchema}]),
-      ],
-    providers: [RelationsService],
-    controllers: [RelationsController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Relations.name, schema: RelationSchema },
+    ]),
+    MongooseModule.forFeature([
+      { name: language.name, schema: languageSchema },
+    ]),
+    MongooseModule.forFeature([{ name: Country.name, schema: CountrySchema }]),
+  ],
+  providers: [RelationsService],
+  controllers: [RelationsController],
 })
 export class RelationsModule {}
